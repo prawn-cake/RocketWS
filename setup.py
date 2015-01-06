0400fdb259487db9197abba453acde1b773d0f11
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2015-01-06 18:37:51 +0800

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='rocketws',
-    version='0.1.0',
+    version='0.1.1',
     packages=['rocketws'],
     url='https://github.com/prawn-cake/RocketWS',
     license='MIT',
